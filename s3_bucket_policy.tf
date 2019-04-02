fa0fcf9bb7cca082962480b6a6a3cbfed4b2fb74
Chris Morgan <chris.morgan@clearcloud.co.uk>
2019-04-02 10:24:14 +0000

    testing

--- s3_bucket_policy.tf
+++ s3_bucket_policy.tf
@@ -4,32 +4,32 @@ resource "aws_s3_bucket_policy" "s3policy" {
 
   policy = <<POLICY
 {
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Sid": "AWSCloudTrail",
-            "Effect": "Allow",
-            "Principal": {
-              "Service": "cloudtrail.amazonaws.com"
-            },
-            "Action": "s3:GetBucketAcl",
-            "Resource": "${aws_s3_bucket.s3.arn}"
-        },
-        {
-            "Sid": "AWSCloudTrailWrite",
-            "Effect": "Allow",
-            "Principal": {
-              "Service": "cloudtrail.amazonaws.com"
-            },
-            "Action": "s3:PutObject",
-            "Resource": "arn:aws:s3:::${aws_s3_bucket.s3.arn}/*",
-            "Condition": {
-                "StringEquals": {
-                    "s3:x-amz-acl": "bucket-owner-full-control"
-                }
-            }
-        }
-    ]
+"Version": "2012-10-17",
+"Statement": [
+{
+"Sid": "AWSCloudTrail",
+"Effect": "Allow",
+"Principal": {
+"Service": "cloudtrail.amazonaws.com"
+},
+"Action": "s3:GetBucketAcl",
+"Resource": "${aws_s3_bucket.s3.arn}"
+},
+{
+"Sid": "AWSCloudTrailWrite",
+"Effect": "Allow",
+"Principal": {
+"Service": "cloudtrail.amazonaws.com"
+},
+"Action": "s3:PutObject",
+"Resource": "arn:aws:s3:::${aws_s3_bucket.s3.arn}/*",
+"Condition": {
+"StringEquals": {
+"s3:x-amz-acl": "bucket-owner-full-control"
+}
+}
+}
+]
 }
 POLICY
 }
