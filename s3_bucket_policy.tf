066dbbece75ee99d055b52885dee25eaaa6a530e
Chris Morgan <chris.morgan@clearcloud.co.uk>
2019-04-02 10:03:33 +0000

    policy needs to be seperate resource

---
+++ s3_bucket_policy.tf
@@ -0,0 +1,37 @@
+resource "aws_s3_bucket_policy" "b" {
+  depends_on = ["aws_s3_bucket.s3"]
+  bucket = "${aws_s3_bucket.s3.id}"
+
+  policy = <<POLICY
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "AWSCloudTrail",
+            "Effect": "Allow",
+            "Principal": {
+              "Service": "cloudtrail.amazonaws.com"
+            },
+            "Action": "s3:GetBucketAcl",
+            "Resource": "$aws_s3_bucket.s3.arn}"
+        },
+        {
+            "Sid": "AWSCloudTrailWrite",
+            "Effect": "Allow",
+            "Principal": {
+              "Service": "cloudtrail.amazonaws.com"
+            },
+            "Action": "s3:PutObject",
+            "Resource": "arn:aws:s3:::${aws_s3_bucket.s3.arn}/*",
+            "Condition": {
+                "StringEquals": {
+                    "s3:x-amz-acl": "bucket-owner-full-control"
+                }
+            }
+        }
+    ]
+}
+
+POLICY
+}
+
