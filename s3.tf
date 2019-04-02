38ec425834e1f6fa37fc3f5c4716431700dfab3c
Chris Morgan <chris.morgan@clearcloud.co.uk>
2019-04-02 10:51:34 +0000

    enable versioning

--- s3.tf
+++ s3.tf
@@ -2,7 +2,7 @@
 # s3 bucket to hold logs
 #
 resource "aws_s3_bucket" "s3" {
-  bucket_prefix = "${var.name}-${var.environment}-securitytrail"
+  bucket_prefix = "${var.name}-${var.environment}-securitytrail-"
 
     versioning {
     enabled = true
