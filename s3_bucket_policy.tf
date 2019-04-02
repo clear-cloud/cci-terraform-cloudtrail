80f9f0f15009b00379a4f1989c63e90d836ba5f2
Chris Morgan <chris.morgan@clearcloud.co.uk>
2019-04-02 10:36:11 +0000

    testing

--- s3_bucket_policy.tf
+++ s3_bucket_policy.tf
@@ -5,24 +5,22 @@ resource "aws_s3_bucket_policy" "s3policy" {
   policy = <<POLICY
 {
 "Version": "2012-10-17",
-"Statement": [
-{
+"Statement":[{
 "Sid": "AWSCloudTrail",
 "Effect": "Allow",
-"Principal": {
+"Principal":{
 "Service": "cloudtrail.amazonaws.com"
 },
 "Action": "s3:GetBucketAcl",
-"Resource": "${aws_s3_bucket.s3.arn}"
-},
-{
+"Resource":"${aws_s3_bucket.s3.arn}"
+},{
 "Sid": "AWSCloudTrailWrite",
 "Effect": "Allow",
-"Principal": {
-"Service": "cloudtrail.amazonaws.com"
+"Principal":{
+"Service":"cloudtrail.amazonaws.com"
 },
 "Action": "s3:PutObject",
-"Resource": "arn:aws:s3:::${aws_s3_bucket.s3.arn}/*",
+"Resource": "${aws_s3_bucket.s3.arn}/*",
 "Condition": {
 "StringEquals": {
 "s3:x-amz-acl": "bucket-owner-full-control"
