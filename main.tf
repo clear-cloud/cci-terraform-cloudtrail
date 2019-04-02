8886b69de5a1c3e01d4b803d7d534a51bc169791
Chris Morgan <chris.morgan@clearcloud.co.uk>
2019-04-02 10:40:38 +0000

    include is_multi_region_trail

--- main.tf
+++ main.tf
@@ -6,4 +6,5 @@ resource "aws_cloudtrail" "trail" {
   name                          = "${var.name}-${var.environment}-trail"
   s3_bucket_name                = "${aws_s3_bucket.s3.id}"
   include_global_service_events = "${var.include_global_service_events}"
+  is_multi_region_trail         = "${var.is_multi_region_trail}" 
 }
