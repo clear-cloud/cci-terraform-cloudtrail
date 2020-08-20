e5c3c0e2d60f139e702d4bd659d5ef287fe72721
Josh Kirkpatrick <josh.kirkpatrick@clearcloud.co.uk>
2020-08-20 16:42:01 +0100

    added AES256 encryption to the bucket

--- s3.tf
+++ s3.tf
@@ -7,4 +7,13 @@ resource "aws_s3_bucket" "s3" {
   versioning {
     enabled = true
   }
+
+    server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        #kms_master_key_id = "${aws_kms_key.htmapps3kmskeydev.arn}"
+        sse_algorithm = "AES256"
+      }
+    }
+  }
 }
